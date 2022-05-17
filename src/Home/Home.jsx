fffaaccc783478f4716efe03710b3b6650b2a539
Franco Vanney <francoavanney@gmail.com>
2022-05-17 15:09:19 -0300

    todos los elementos

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import '../Home/Home.css'
 import logo from '../assets/Home/logo.svg'
+import proximamente from '../assets/Home/proximamente.svg'
+import somosmusica from '../assets/Home/somos-musica.svg'
 
 function Home() {
         return (
         <div className="container">
             <h1>holi</h1>
             <div className="overlap-group">
-            <img className="logo" src={logo}/>
-            <img className="proximamente" src="proximamente.svg" />
-            <img className="somos-musica" src="somos-musica.svg" />
-            <img className="e-mail" src="e-mail.svg" />
+                <img className="logo" src={logo}/>
+                <img className="proximamente" src={proximamente} />
+                <img className="somos-musica" src={somosmusica}/>
+                <span id='mail'>info@2222agencia.com</span>
             </div>
         </div>
         );
