94727e5cba0d78b42ebdb8e3dcd66eec0e48b516
Franco Vanney <francoavanney@gmail.com>
2022-05-17 19:29:26 -0300

    saque animaciones

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -10,9 +10,9 @@ function Home() {
         return (
         <>
                 <div className="container-home">
-                                <img className="px-3 logo img-fluid animate__animated animate__bounce animate__slow animate__delay-2s" src={Logo22}/>
-                                <img className="px-3 proximamente img-fluid animate__animated animate__flash animate__slower animate__delay-3s animate__infinite" src={proximamente} />
-                                <img className="somos-musica img-fluid animate__animated animate__pulse animate__slower animate__delay-3s" src={somosmusica}/>
+                                <img className="px-3 logo img-fluid " src={Logo22}/>
+                                <img className="px-3 proximamente img-fluid" src={proximamente} />
+                                <img className="somos-musica img-fluid" src={somosmusica}/>
                                 <a href='mailto:info@2222agencia.com' id='mail' target="_blank">info@2222agencia.com</a>
                 </div>
                 <div className='text-center mb-4'> <a href="https://pampacode.com/" target="_blank"><img src={LogoPampa} width="120" alt="Pampa code" /></a></div>
