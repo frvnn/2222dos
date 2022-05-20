51b2ea36f20889f9a0c5cf35908ec64dcda0efc6
Santiago Corbanini <santiagocorbanini@gmail.com>
2022-05-20 10:51:04 +0200

    Pongo el logo en horizontal

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
                         <div className="container-home">
                                 <div className='logo-container'>
                                         <img className="px-3 m-2 px-lg-0 logo img-fluid " src={Veintidos22}/>
-                                        <img className="px-lg-3 m-lg-3 dots img-fluid animate__animated animate__flash animate__slow animate__delay-2s animate__infinite" src={dots}/>
+                                        <img className="px-lg-3 m-lg-3 dots img-fluid animate__animated animate__flash animate__slow animate__delay-5s animate__infinite" src={dots}/>
                                         <img className="px-3 m-2 px-lg-0 logo img-fluid " src={Veintidos22}/>
                                 </div>
                                         
